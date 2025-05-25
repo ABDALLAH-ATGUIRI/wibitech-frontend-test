eeed07cf19532f9f5b72a79981dc22608f7372fe
ABDALLAH-ATGUIRI <abdallah.atguiri@gmail.com>
2025-05-25 03:09:53 +0100

    fix: update favicon reference in index.html and adjust logo component image source

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@
 const Logo = () => (
   <div className="flex items-center gap-[9px]">
     <div className="relative w-7 h-7 bg-[#007fff] rounded-[25%] flex items-center justify-center">
-      <img className="w-4 h-3" alt="Vector" src="/vector.svg" />
+      <img className="w-4 h-3" alt="Vector" src="vector.svg" />
     </div>
     <span className="text-lg">Taski</span>
   </div>
